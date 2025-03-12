43eb0667f2ab4fa69347f6564eb9f9aee996008e
mr-sean456 <sean.cruz3@gmail.com>
2025-03-12 17:37:05 +0530

    Add files via upload

---
+++ js/widget.js
@@ -0,0 +1,4 @@
+/*
+ Copyright (c) 2016 Feedity (feedity.com), All rights reserved */
+""!=feedity_widget_feed&&""!=feedity_widget_numberofitems&&""!=feedity_widget_showdate&&""!=feedity_widget_width&&""!=feedity_widget_backcolor&&""!=feedity_widget_fontcolor&&(feedity_widget_showdescription||(feedity_widget_showdescription="no"),document.write('<script type="text/javascript" src="https://feedity.com/widget.aspx?fu='+encodeURIComponent(feedity_widget_feed)+"&nm="+encodeURIComponent(feedity_widget_numberofitems)+"&dc="+encodeURIComponent(feedity_widget_showdescription)+"&dt="+encodeURIComponent(feedity_widget_showdate)+
+"&wd="+encodeURIComponent(feedity_widget_width)+"&bc="+encodeURIComponent(feedity_widget_backcolor)+"&fc="+encodeURIComponent(feedity_widget_fontcolor)+'">\x3c/script>'));
